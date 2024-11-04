477f1d6bc8fae399cca899587b8dd530e3743ca3
robert.ibram <robert.ibram@accesa.eu>
2024-11-04 19:45:58 +0200

    Implement service

---
+++ src/main/java/guru/springframework/springaiintro/services/OpenAIService.java
@@ -0,0 +1,6 @@
+package guru.springframework.springaiintro.services;
+
+public interface OpenAIService {
+
+	String getAnswer(String question);
+}
---
+++ src/test/java/guru/springframework/springaiintro/services/OpenAIServiceImplTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.springaiintro.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class OpenAIServiceImplTest {
+
+	@Autowired
+	OpenAIService openAIService;
+
+	@Test
+	void getAnswer() {
+
+		String answer = openAIService.getAnswer("Write a python script to output numbers from 1 to 100");
+		System.out.println(answer);
+	}
+}
